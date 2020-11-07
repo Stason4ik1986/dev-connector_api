ac65d2c9c088e6d234c26bfdde9bdebe02e3cea0
Stanislav <first1986st@gmail.com>
2020-11-07 14:02:43 +0200

    Create API

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-// mongodb+srv://stas_cherednichenko:<password>@devconnector.yrto7.mongodb.net/<dbname>?retryWrites=true&w=majority
+// Connect Database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
